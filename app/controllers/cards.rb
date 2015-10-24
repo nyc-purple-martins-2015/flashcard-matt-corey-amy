4a996f9bae6a748bb805ae681bf09084911a881a
Amy Liao & Corey & Matthew LeFurge <pair+amyhliao.cgrad01.mlefurge@devbootcamp.com>
2015-10-23 20:41:20 -0400

    add round logic, relationship do not work

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -1,22 +1,27 @@
 get '/card/:id' do
   @card = Card.find(params[:id])
   @deck = Deck.find_by(id: "#{@card.deck_id}")
-  @round = Round.find_by()
+  @round = Round.find_by(deck_id: "#{@deck.id}")
   erb :'cards/view_card'
 end
 
 post '/card/:id' do
+  @next_card = params[:id].to_i + 1 #comment this out
   @card = Card.find(params[:id])
-  @guess = Guess.create!(card_id: params[:id], round_id: 1) #What do we need to do with round_id
+  @deck = Deck.find_by(id: "#{@card.deck_id}")
+  @round = Round.find_by(deck_id: "#{@deck.id}")
+  @guess = Guess.create!(card_id: params[:id], round_id: "#{@round.id}")
   @count = @guess.count
   @answer = params[:answer]
   if @card.answer == @answer
     @count += 1
     @guess.update_attributes(correctness: true, count: @count)
-    redirect "/" #we need to redirect to random card that has not been correctly answered
+    redirect "/card/#{@next_card}" #we need to redirect to random card that has not been correctly answered
   else
     @count += 1
     @guess.update_attributes(count: @count)
     redirect "/"
   end
 end
+
+
