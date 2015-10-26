598b5a0519924b5ffd542b90e6f6bb99e7864faf
Amy Liao <amyhliao88@gmail.com>
2015-10-25 18:30:45 -0700

    fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :rounds
   has_many :guesses, through: :rounds
   has_many :decks, through: :rounds
 
-  has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 
   def count_rounds
     self.rounds.count
